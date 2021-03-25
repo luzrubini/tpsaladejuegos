a2edaeb6293e256f7a0531fa8bbb924892565f07
Luz Rubini <luzrubini@hotmail.com>
2021-03-25 15:16:54 -0300

    home changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { QuiensoyComponent } from './componentes/quiensoy/quiensoy.component';
+import { HomeComponent } from './componentes/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    QuiensoyComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
